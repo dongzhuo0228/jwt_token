08785829d807c9adfda9715f98ff5a9990a39988
董茁 <dongz001928@yimidida.com>
2019-07-22 17:39:35 +0800

    替换jar

--- src/main/java/com/example/jwt_token/config/MybatisPlusConfig.java
+++ src/main/java/com/example/jwt_token/config/MybatisPlusConfig.java
@@ -1,7 +1,7 @@
 package com.example.jwt_token.config;
 
-import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
-import com.baomidou.mybatisplus.plugins.PerformanceInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.PerformanceInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
